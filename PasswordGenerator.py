cd5964dad54440c4a94eb5e8bcf39e755f0155e0
2Gans2 <silinskiy.gans@gmail.com>
2026-02-09 15:55:59 +0300

    Update PasswordGenerator.py

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -5,7 +5,7 @@ import random
 
 
 st.title("Генератор паролей")
-length = st.slider("Длинна", 8, 24, 16)
+length = st.slider("Длинна пароля", 8, 24, 16)
 special_characters = st.checkbox("Использовать спецсимволы")
 if st.button("Создать"):
     characters = ascii_letters + digits
@@ -13,4 +13,5 @@ if st.button("Создать"):
         characters += punctuation
     password = "".join(random.choices(characters, k=length))
     st.code(password, language="text")
+
 
